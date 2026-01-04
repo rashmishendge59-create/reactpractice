6c7713e30017d553cf8c944c51ac8a76353726e5
rashmisumhr <69501895+rashmisumhr@users.noreply.github.com>
2026-01-04 13:27:08 +0530

    episode - 01

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am an H1 tag"),
+        React.createElement("h2",{},"I am an H2 tag")
+    ]
+    ),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am an H1 tag"),
+        React.createElement("h2",{},"I am an H2 tag")
+    ]
+    )]
+)
+
+
+//const heading = React.createElement("h1",{id:"heading", xyz:"hi"},"Hello from React");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
