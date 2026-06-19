e4a3313531bf4109bf78c9ea563d2d3ef9c04550
rashmisumhr <69501895+rashmisumhr@users.noreply.github.com>
2026-06-19 12:21:47 +0530

    Igniting our app
    Installed parcel

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I am an H1 tag"),
+        React.createElement("h1",{},"I am an H1 tag ***"),
         React.createElement("h2",{},"I am an H2 tag")
     ]
     ),
