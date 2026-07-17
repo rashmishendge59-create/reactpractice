40e398af6e640df4705671bf8dfa14b74562544c
rashmisumhr <69501895+rashmisumhr@users.noreply.github.com>
2026-07-18 01:05:23 +0530

    Accordian
    Accordian created for restaraurant menus

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,3 +1,5 @@
+import { CDN_URL } from "../utils/constants"
+
 const ItemList = ({items}) => {
     console.log(items)
     return (
@@ -6,11 +8,34 @@ const ItemList = ({items}) => {
                 
             <div 
             key={item.card.info.id} 
-            className="p-2 m-2 border-gray-300 border-b-2">
+            className="p-2 m-2 border-gray-200 border-b-2 text-left">
+                <div><img src = {CDN_URL+item.card?.info?.imageId}></img></div>
+
                 <div>
-                    <span>{item.card?.info?.name}</span>
-                    <span>{item.card?.info?.description}</span>
-                </div>
+                    <span>{item.card?.info?.isVeg === 1 ? "🍀 " : "🔺 "}</span>
+                    <span className="text-lg font-bold">{item.card?.info?.name}</span>
+
+                    <div className="flex items-center gap-2 font-bold">
+                        {item.card?.info?.finalPrice ? (
+                        <>
+                            <span className="line-through text-gray-500">
+                            ₹{item.card?.info?.price / 100}
+                            </span>
+
+                            <span className="font-semibold text-green-600">
+                            ₹{item.card?.info?.finalPrice / 100}
+                            </span>
+                        </>
+                        ) : (
+                        <span>
+                            ₹{item.card?.info?.price / 100}
+                        </span>
+                        )}
+                    </div>
+                    <span>❇️ {item.card?.info?.ratings?.aggregatedRating?.rating }</span>
+                    <span> ({item.card?.info?.ratings?.aggregatedRating?.ratingCountV2 })</span>
+                </div>  
+                <p>{item.card?.info?.description}</p>
             </div>
             ))}
         </div>
--- src/components/RestraurantCategory.js
+++ src/components/RestraurantCategory.js
@@ -3,8 +3,8 @@ import ItemList from "./ItemList";
 const RestraurantCategory = ({data}) => {
     console.log(data);
     return (
-        <div className="flex justify-between">
-            <div className="mx-auto my-4 w-6/12 bg-gray-50 shadow-lg p-4  font-bold text-lg">
+        <div className="mx-auto my-4 w-6/12 ">
+            <div className="flex justify-between bg-gray-50 shadow-lg p-4  font-bold text-lg">
                 <span>{data.title}({data.itemCards.length})</span>
                 <span>⬇️</span>
             </div>
